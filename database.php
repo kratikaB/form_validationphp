221fa568eaa4ba585c4dde86f131a432c471bae3
user <kratika.bahgat.ets@gmail.com>
2021-04-12 23:03:48 +0530

    working

--- database.php
+++ database.php
@@ -1,102 +1,102 @@
- <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Personal details</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-        table tr td:last-child{
-            width: 120px;
-        }
-    </style>
-    <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
-</head>
-<body>
-	 <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="mt-5 mb-3 clearfix">
-                        <h2 class="heading">Personal details</h2>
- <?php
-        include"conn.php";
-          
-        // Taking all 5 values from the form data(input)
-        $name =  $_REQUEST['name'];
-        $email = $_REQUEST['email'];
-        $website = $_REQUEST['website'];
-        $address = $_REQUEST['address'];
-        $gender = $_REQUEST['gender'];
-        $password = generate_password();
- 		$encpt_password= ($enc_pass);
-
-          
-        // Performing insert query execution
-            $sql = "INSERT INTO form_data (name, email, website, address, gender, password) VALUES ('$name', '$email','$website','$address','$gender' , '$password')";
-
-          
-        if(mysqli_query($conn, $sql)){
-
-        } else{
-            echo "ERROR: Hush! Sorry $sql. " 
-                . mysqli_error($conn);
-        }
-        $sql = "SELECT * FROM form_data";
-                    if($result = mysqli_query($conn, $sql)){
-                        if(mysqli_num_rows($result) > 0){
-                            echo '<table class="table table-bordered table-striped">';
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th>Name</th>";
-                                        echo "<th>Email</th>";
-                                        echo "<th>website</th>";
-                                        echo "<th>Address</th>";
-                                        echo "<th>Gender</th>";
-                                    echo "</tr>";
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = mysqli_fetch_array($result)){
-                                    echo "<tr>";
-                                        echo "<td>" . $row['name'] . "</td>";
-                                        echo "<td>" . $row['email'] . "</td>";
-                                        echo "<td>" . $row['website'] . "</td>";
-                                        echo "<td>" . $row['address'] . "</td>";
-                                        echo "<td>" . $row['gender'] . "</td>";
-                                        echo "<td>";
-                                            echo '<a href="update1.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
-                                        echo "</td>";
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";                            
-                            echo "</table>";
-                            // Free result set
-                            mysqli_free_result($result);
-                        } else{
-                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-                        }
-                    } else{
-                        echo "Oops! Something went wrong. Please try again later.";
-                    }
-
-
-        // Close connection
-        mysqli_close($conn);
-        ?>
-</div>
-            </div>        
-        </div>
-    </div>
-</body>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>Personal details</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <style>
+         .wrapper{
+         width: 600px;
+         margin: 0 auto;
+         }
+         table tr td:last-child{
+         width: 120px;
+         }
+      </style>
+      <script>
+         $(document).ready(function(){
+             $('[data-toggle="tooltip"]').tooltip();   
+         });
+      </script>
+   </head>
+   <body>
+      <div class="wrapper">
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-12">
+               <div class="mt-5 mb-3 clearfix">
+                  <h2 class="heading">Personal details</h2>
+                  <?php
+                     include"conn.php";
+                       
+                     // Taking all 5 values from the form data(input)
+                     $name =  $_REQUEST['name'];
+                     $email = $_REQUEST['email'];
+                     $website = $_REQUEST['website'];
+                     $address = $_REQUEST['address'];
+                     $gender = $_REQUEST['gender'];
+                     $password = generate_password();
+                     $encpt_password= ($enc_pass);
+                     
+                       
+                     // Performing insert query execution
+                         $sql = "INSERT INTO form_data (name, email, website, address, gender, password) VALUES ('$name', '$email','$website','$address','$gender' , '$password')";
+                     
+                       
+                     if(mysqli_query($conn, $sql)){
+                     
+                     } else{
+                         echo "ERROR: Hush! Sorry $sql. " 
+                             . mysqli_error($conn);
+                     }
+                     $sql = "SELECT * FROM form_data";
+                                 if($result = mysqli_query($conn, $sql)){
+                                     if(mysqli_num_rows($result) > 0){
+                                         echo '<table class="table table-bordered table-striped">';
+                                             echo "<thead>";
+                                                 echo "<tr>";
+                                                     echo "<th>Name</th>";
+                                                     echo "<th>Email</th>";
+                                                     echo "<th>website</th>";
+                                                     echo "<th>Address</th>";
+                                                     echo "<th>Gender</th>";
+                                                 echo "</tr>";
+                                             echo "</thead>";
+                                             echo "<tbody>";
+                                             while($row = mysqli_fetch_array($result)){
+                                                 echo "<tr>";
+                                                     echo "<td>" . $row['name'] . "</td>";
+                                                     echo "<td>" . $row['email'] . "</td>";
+                                                     echo "<td>" . $row['website'] . "</td>";
+                                                     echo "<td>" . $row['address'] . "</td>";
+                                                     echo "<td>" . $row['gender'] . "</td>";
+                                                     echo "<td>";
+                                                         echo '<a href="update1.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                                         echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                                     echo "</td>";
+                                                 echo "</tr>";
+                                             }
+                                             echo "</tbody>";                            
+                                         echo "</table>";
+                                         // Free result set
+                                         mysqli_free_result($result);
+                                     } else{
+                                         echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                                     }
+                                 } else{
+                                     echo "Oops! Something went wrong. Please try again later.";
+                                 }
+                     
+                     
+                     // Close connection
+                     mysqli_close($conn);
+                     ?>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
 </html>
--- update1.php
+++ update1.php
@@ -1,164 +1,163 @@
 <?php
-// Include config file
-include "conn.php";
- 
-// Define variables and initialize with empty values
-$nameErr = $emailErr = $genderErr = $address_err = $websiteErr = "";
-$name = $email = $gender = $address = $website = "";
- 
-// Processing form data when form is submitted
-if(isset($_POST["id"]) && !empty($_POST["id"])){
-    // Get hidden input value
-    $id = $_POST["id"];
+   // Include config file
+   include "conn.php";
     
-    // Validate name
-    $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid name.";
-    } else{
-        $name = $input_name;
-    }
+   // Define variables and initialize with empty values
+   $nameErr = $emailErr = $genderErr = $address_err = $websiteErr = "";
+   $name = $email = $gender = $address = $website = "";
     
-    // Validate address address
-    $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $address_err = "Please enter an address.";     
-    } else{
-        $address = $input_address;
-    }
-    
-   
-    // Check input errors before inserting in database
-    if(empty($name_err) && empty($address_err) && empty($emailErr)) && empty($websiteErr)) && empty($genderErr)
-        // Prepare an update statement
-        $sql = "UPDATE form_data SET name=?, email=?, address=?, website=?, gender=? WHERE id=?";
-         
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssi", $param_name, $param_email $param_address, $param_website, $param_gender, $param_id);
-            
-            // Set parameters
-            $param_name = $name;
-            $param_email =$email;
-            $param_address = $address;
-            $param_website = $website;
-            $param_gender = $gender;
-            $param_id = $id;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Records updated successfully. Redirect to landing page
-                header("location: database.php");
-                exit();
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-    
-    // Close connection
-    mysqli_close($conn);
-} else{
-    // Check existence of id parameter before processing further
-    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-        // Get URL parameter
-        $id =  trim($_GET["id"]);
-        
-        // Prepare a select statement
-        $sql = "SELECT * FROM employees WHERE id = ?";
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $param_id);
-            
-            // Set parameters
-            $param_id = $id;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_get_result($stmt);
-    
-                if(mysqli_num_rows($result) == 1){
-                    /* Fetch result row as an associative array. Since the result set
-                    contains only one row, we don't need to use while loop */
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                    
-                    // Retrieve individual field value
-                    $name = $row["name"];
-                    $email = $row["email"];
-                    $website = $row["website"];
-                    $address = $row["address"];
-                    $gender = $row["gender"];
-                } else{
-                    // URL doesn't contain valid id. Redirect to error page
-                    exit();
-                }
-                
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-        
-        // Close statement
-        mysqli_stmt_close($stmt);
-        
-        // Close connection
-        mysqli_close($conn);
-    }  else{
-        // URL doesn't contain id parameter. Redirect to error page
+   // Processing form data when form is submitted
+   if(isset($_POST["id"]) && !empty($_POST["id"])){
+       // Get hidden input value
+       $id = $_POST["id"];
        
-        exit();
-    }
-}
-?>
- 
+       // Validate name
+       $input_name = trim($_POST["name"]);
+       if(empty($input_name)){
+           $name_err = "Please enter a name.";
+       } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+           $name_err = "Please enter a valid name.";
+       } else{
+           $name = $input_name;
+       }
+       
+       // Validate address address
+       $input_address = trim($_POST["address"]);
+       if(empty($input_address)){
+           $address_err = "Please enter an address.";     
+       } else{
+           $address = $input_address;
+       }
+       
+      
+       // Check input errors before inserting in database
+       if(empty($name_err) && empty($address_err) && empty($emailErr)) && empty($websiteErr)) && empty($genderErr)
+           // Prepare an update statement
+           $sql = "UPDATE form_data SET name=?, email=?, address=?, website=?, gender=? WHERE id=?";
+            
+           if($stmt = mysqli_prepare($conn, $sql)){
+               // Bind variables to the prepared statement as parameters
+               mysqli_stmt_bind_param($stmt, "sssi", $param_name, $param_email $param_address, $param_website, $param_gender, $param_id);
+               
+               // Set parameters
+               $param_name = $name;
+               $param_email =$email;
+               $param_address = $address;
+               $param_website = $website;
+               $param_gender = $gender;
+               $param_id = $id;
+               
+               // Attempt to execute the prepared statement
+               if(mysqli_stmt_execute($stmt)){
+                   // Records updated successfully. Redirect to landing page
+                   header("location: database.php");
+                   exit();
+               } else{
+                   echo "Oops! Something went wrong. Please try again later.";
+               }
+           }
+            
+           // Close statement
+           mysqli_stmt_close($stmt);
+       }
+       
+       // Close connection
+       mysqli_close($conn);
+   } else{
+       // Check existence of id parameter before processing further
+       if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+           // Get URL parameter
+           $id =  trim($_GET["id"]);
+           
+           // Prepare a select statement
+           $sql = "SELECT * FROM employees WHERE id = ?";
+           if($stmt = mysqli_prepare($conn, $sql)){
+               // Bind variables to the prepared statement as parameters
+               mysqli_stmt_bind_param($stmt, "i", $param_id);
+               
+               // Set parameters
+               $param_id = $id;
+               
+               // Attempt to execute the prepared statement
+               if(mysqli_stmt_execute($stmt)){
+                   $result = mysqli_stmt_get_result($stmt);
+       
+                   if(mysqli_num_rows($result) == 1){
+                       /* Fetch result row as an associative array. Since the result set
+                       contains only one row, we don't need to use while loop */
+                       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                       
+                       // Retrieve individual field value
+                       $name = $row["name"];
+                       $email = $row["email"];
+                       $website = $row["website"];
+                       $address = $row["address"];
+                       $gender = $row["gender"];
+                   } else{
+                       // URL doesn't contain valid id. Redirect to error page
+                       exit();
+                   }
+                   
+               } else{
+                   echo "Oops! Something went wrong. Please try again later.";
+               }
+           }
+           
+           // Close statement
+           mysqli_stmt_close($stmt);
+           
+           // Close connection
+           mysqli_close($conn);
+       }  else{
+           // URL doesn't contain id parameter. Redirect to error page
+          
+           exit();
+       }
+   }
+   ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Update Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <div class="container-fluid">
+   <head>
+      <meta charset="UTF-8">
+      <title>Update Record</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <style>
+         .wrapper{
+         width: 600px;
+         margin: 0 auto;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5">Update Record</h2>
-                    <p>Please edit the input values and submit to update the employee record.</p>
-                    <form action="database.php" method="post">
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
-                            <span class="invalid-feedback"><?php echo $name_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Address</label>
-                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
-                            <span class="invalid-feedback"><?php echo $address_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Salary</label>
-                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
-                            <span class="invalid-feedback"><?php echo $salary_err;?></span>
-                        </div>
-                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
-                    </form>
-                </div>
-            </div>        
-        </div>
-    </div>
-</body>
+               <div class="col-md-12">
+                  <h2 class="mt-5">Update Record</h2>
+                  <p>Please edit the input values and submit to update the employee record.</p>
+                  <form action="database.php" method="post">
+                     <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                        <span class="invalid-feedback"><?php echo $name_err;?></span>
+                     </div>
+                     <div class="form-group">
+                        <label>Address</label>
+                        <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                        <span class="invalid-feedback"><?php echo $address_err;?></span>
+                     </div>
+                     <div class="form-group">
+                        <label>Salary</label>
+                        <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                        <span class="invalid-feedback"><?php echo $salary_err;?></span>
+                     </div>
+                     <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                     <input type="submit" class="btn btn-primary" value="Submit">
+                     <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
 </html>
